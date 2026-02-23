841895f7b760de2a2944078d046d36e74b170a74
vaghelaankit044-ai <vaghelaankit044@gmail.com>
2026-02-23 22:57:55 +0530

    Add files via upload

---
+++ p7.py
@@ -0,0 +1,28 @@
+i=1
+while(i<=5):
+    print(i)
+    i=i+1
+
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+
+no=int(input("enter sto number :"))
+
+sum=0
+i=1
+while(i<=no):
+    sum=sum+i
+    print(i,"\t\t",sum)
+    i=i+1
+
+num=int(input("enter number :"))
+
+i=1
+while(i<=10):
+    print(f"{num} * {i} = {num * i}")
+    i=i+1
+    
+
+
