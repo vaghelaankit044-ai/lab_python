ec055cd57d4afdef0dab989d862d8ab4671a7092
vaghelaankit044-ai <vaghelaankit044@gmail.com>
2026-03-24 18:46:05 +0530

    Add files via upload

---
+++ arr.py
@@ -0,0 +1,25 @@
+arr=[10,20,30,40]
+for i in arr:
+    print(i)
+
+arr=[10,20,30]
+arr.insert(1,15)
+print(arr)
+
+ar=[10,20,30,40]
+arr.remove(20)
+print(arr)
+
+arr=[10,20,30,40]
+if 30 in arr:
+    print("found")
+else:
+    print("not found")
+
+    arr=[10,20,30]
+    arr[1]=25
+    print(arr)
+
+    arr=[40,10,30,20]
+    arr.sort()
+    print(arr)
