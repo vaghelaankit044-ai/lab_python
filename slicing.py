69a561c254ab503408e6805c7a18dc3d538b254b
vaghelaankit044-ai <vaghelaankit044@gmail.com>
2026-03-24 19:30:40 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,14 @@
+from array import*
+arr=array('i',[10,20,30,40,50,60,])
+print(arr[1:4])
+
+print(arr[:3])
+print(arr[2:])
+print(arr[:])
+print(arr[::2])
+print(arr[::3])
+print(arr[1::2])
+
+print(arr[-4:1])
+print(arr[-3:])
+print(arr[:-2])
